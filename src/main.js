21b83fcddaeca64441aaee85bf097cb6c6b182a3
christianarca27 <christian.arca.mail@gmail.com>
2023-06-07 09:38:58 +0200

    feat: installato FontAwesome

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import './style.scss'
 import App from './App.vue'
 import * as bootstrap from 'bootstrap'
+import '@fortawesome/fontawesome-free/css/all.css'
 
 import { router } from './router'
 
