8b3e75d64bc4018b69dce81acb92afdd2181c652
christianarca27 <christian.arca.mail@gmail.com>
2023-06-07 09:39:23 +0200

    feat: modificato footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -12,11 +12,37 @@ export default {
 
 <template>
     <footer class="bg-body-tertiary">
-        <div class="container-fluid py-3 text-center">
-            <p>Copyright&copy; 2023 Christian Arca</p>
-            <p>Designed by Christian Arca</p>
+        <div class="container d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+            <div class="col-md-4 d-flex align-items-center">
+                <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
+                    logo
+                </a>
+                <span class="mb-3 mb-md-0 text-muted">© 2023 Christian Arca</span>
+            </div>
+
+            <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+                <li class="ms-3">
+                    <a class="text-muted" href="#">
+                        <i class="fa-brands fa-twitter"></i>
+                    </a>
+                </li>
+                <li class="ms-3">
+                    <a class="text-muted" href="#">
+                        <i class="fa-brands fa-instagram"></i>
+                    </a>
+                </li>
+                <li class="ms-3">
+                    <a class="text-muted" href="#">
+                        <i class="fa-brands fa-facebook"></i>
+                    </a>
+                </li>
+            </ul>
         </div>
     </footer>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+i {
+    font-size: 1.5rem;
+}
+</style>
