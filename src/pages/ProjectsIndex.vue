0007c8da07f990475ed1feca0e8d18433b1e3867
christianarca27 <christian.arca.mail@gmail.com>
2023-06-07 12:28:58 +0200

    feat: gestione della paginazione

--- src/pages/ProjectsIndex.vue
+++ src/pages/ProjectsIndex.vue
@@ -18,6 +18,16 @@ export default {
             error: '',
 
             filteredCategoryId: '',
+
+            currentPage: 1,
+
+            totalPages: 1,
+
+            totalRes: 0,
+
+            nextPageUrl: '',
+
+            prevPageUrl: '',
         }
     },
 
@@ -26,17 +36,22 @@ export default {
             let queryUrl = '';
 
             if (this.filteredCategoryId) {
-                queryUrl = 'http://127.0.0.1:8000/api/projects?type_id=' + this.filteredCategoryId;
+                queryUrl = 'http://127.0.0.1:8000/api/projects?page=' + this.currentPage + '&type_id=' + this.filteredCategoryId;
             }
             else {
-                queryUrl = 'http://127.0.0.1:8000/api/projects';
+                queryUrl = 'http://127.0.0.1:8000/api/projects?page=' + this.currentPage;
             }
 
             axios.get(queryUrl).then(res => {
+                console.log(res);
                 this.querySuccess = res.data.success;
 
                 if (this.querySuccess) {
-                    this.projects = res.data.results;
+                    this.projects = res.data.results.data;
+
+                    this.currentPage = res.data.results.current_page;
+                    this.totalPages = res.data.results.last_page;
+                    this.totalRes = res.data.results.total;
                     this.types = res.data.types;
                 }
                 else {
@@ -45,6 +60,30 @@ export default {
 
                 this.queryReady = true;
             });
+        },
+
+        nextPage() {
+            if (this.currentPage < this.totalPages) {
+                this.currentPage++;
+
+                this.getProjects();
+            }
+        },
+
+        prevPage() {
+            if (this.currentPage > 1) {
+                this.currentPage--;
+
+                this.getProjects();
+            }
+        },
+
+        setPage(page) {
+            if (this.currentPage != page) {
+                this.currentPage = page;
+
+                this.getProjects();
+            }
         },
     },
 
@@ -62,9 +101,9 @@ export default {
     <main>
         <div v-if="queryReady" class="container py-5">
             <div v-if="querySuccess" id="projects">
-                <h1>Lista progetti</h1>
+                <h1>Progetti</h1>
 
-                <div class="input-group">
+                <div class="input-group mb-3">
                     <select @change="getProjects()" class="form-select" v-model="filteredCategoryId">
                         <option value="" selected>Tutti</option>
 
@@ -72,11 +111,32 @@ export default {
                     </select>
                 </div>
 
-                <div class="row">
-                    <div class="col-12 col-md-6 col-lg-4 col-xl-3 g-4" v-for="project in projects">
+                <pre>{{ totalRes }} risultati trovati</pre>
+
+                <div class="row mb-3">
+                    <div class="col-12 col-md-6 col-lg-4 g-4" v-for="project in projects">
                         <ProjectCard :project="project"></ProjectCard>
                     </div>
                 </div>
+
+                <ul class="pagination d-flex justify-content-center">
+                    <li class="page-item" :class="currentPage > 1 ? '' : 'disabled'">
+                        <button @click="prevPage()" class="page-link">
+                            <i class="fa-solid fa-chevron-left"></i>
+                        </button>
+                    </li>
+
+                    <li v-for="page in totalPages" class="page-item" :class="currentPage == page ? 'active' : ''">
+                        <button @click="setPage(page)" class="page-link" :class="currentPage == page ? 'active' : ''">{{
+                            page }}</button>
+                    </li>
+
+                    <li class="page-item" :class="currentPage < totalPages ? '' : 'disabled'">
+                        <button @click="nextPage()" class="page-link">
+                            <i class="fa-solid fa-chevron-right"></i>
+                        </button>
+                    </li>
+                </ul>
             </div>
 
             <div v-else class="alert alert-info" role="alert">
